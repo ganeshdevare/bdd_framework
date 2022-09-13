2f2eae93f3cb0d774de9568b7b7a9317de023a0d
Ganesh Devare <Ganesh.Devare@mindbodyonline.com>
2022-09-13 10:47:22 +0530

    living doc emergency info

--- SpecFlowAutomation/Features/EmegencyInfo.feature
+++ SpecFlowAutomation/Features/EmegencyInfo.feature
@@ -9,14 +9,14 @@ Scenario Outline: Add Emergency Contact
 	And I enter password as "<password>"
 	And I click on login
 	And I click on My Info
-	And I click on Emergency Contacts 
+	And I click on Emergency Contacts
 	And I click on Add
 	And I fill the add emergency contact section
-		| name   | relationship   | home_telephone   | mobile   | work_telephone   | 
-		| <name> | <relationship> | <home_telephone> | <mobile> | <work_telephone> | 
+		| name   | relationship   | home_telephone   | mobile   | work_telephone   |
+		| <name> | <relationship> | <home_telephone> | <mobile> | <work_telephone> |
 	And I click on save emergency contact
 	Then I should see add emergency contact record under assigned emergency contacts
 Examples:
-      | username | password | name   | relationship | home_telephone | mobile       | work_telephone | 
-      | ramesh123 | Ramesh@123 | Ram | Father       | 2882010        | 23432   | 23432        | 
-      | ramesh124 | Ramesh@123 | Sita | Mother       | 2882010        | 32432   | 32432        | 
+	| username  | password   | name | relationship | home_telephone | mobile | work_telephone |
+	| ramesh123 | Ramesh@123 | Ram  | Father       | 2882010        | 23432  | 23432          |
+	| ramesh124 | Ramesh@123 | Sita | Mother       | 2882010        | 32432  | 32432          |
--- SpecFlowAutomation/StepDefinitions/EmergencyContactsStepDefinitions.cs
+++ SpecFlowAutomation/StepDefinitions/EmergencyContactsStepDefinitions.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using SpecFlowAutomation.Hooks;
 using System;
 using TechTalk.SpecFlow;
 
@@ -6,40 +9,52 @@ namespace SpecFlowAutomation.StepDefinitions
     [Binding]
     public class EmergencyContactsStepDefinitions
     {
+        string name;
         [When(@"I click on My Info")]
         public void WhenIClickOnMyInfo()
         {
-            throw new PendingStepException();
+            AutomationHooks.driver.FindElement(By.XPath("//span[text()='My Info']")).Click();
         }
 
         [When(@"I click on Emergency Contacts")]
         public void WhenIClickOnEmergencyContacts()
         {
-            throw new PendingStepException();
+            AutomationHooks.driver.FindElement(By.XPath("//a[text()='Emergency Contacts']")).Click();
         }
 
         [When(@"I click on Add")]
         public void WhenIClickOnAdd()
         {
-            throw new PendingStepException();
+            AutomationHooks.driver.FindElement(By.XPath("//h6[text()='Assigned Emergency Contacts']/following::button")).Click();
         }
 
         [When(@"I fill the add emergency contact section")]
         public void WhenIFillTheAddEmergencyContactSection(Table table)
         {
-            throw new PendingStepException();
+            name = table.Rows[0]["name"];
+            string relationship = table.Rows[0]["relationship"];
+            string home_telephone = table.Rows[0]["home_telephone"];
+            string mobile = table.Rows[0]["mobile"];
+            string work_telephone = table.Rows[0]["work_telephone"];
+
+            AutomationHooks.driver.FindElement(By.XPath("//label[contains(text(),'Name')]/following::input")).SendKeys(name);
+            AutomationHooks.driver.FindElement(By.XPath("//label[contains(text(),'Relationship')]/following::input")).SendKeys(relationship);
+            AutomationHooks.driver.FindElement(By.XPath("//label[contains(text(),'Home Telephone')]/following::input")).SendKeys(home_telephone);
+            AutomationHooks.driver.FindElement(By.XPath("//label[contains(text(),'Mobile')]/following::input")).SendKeys(mobile);
+            AutomationHooks.driver.FindElement(By.XPath("//label[contains(text(),'Work Telephone')]/following::input")).SendKeys(work_telephone);
+
         }
 
         [When(@"I click on save emergency contact")]
         public void WhenIClickOnSaveEmergencyContact()
         {
-            throw new PendingStepException();
+            AutomationHooks.driver.FindElement(By.XPath("//button[@type='submit']")).Click();
         }
 
         [Then(@"I should see add emergency contact record under assigned emergency contacts")]
         public void ThenIShouldSeeAddEmergencyContactRecordUnderAssignedEmergencyContacts()
         {
-            throw new PendingStepException();
+
         }
     }
 }
